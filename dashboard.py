f9679ce9b8fcdd82e57141197229a82f6889f882
harlock024 <hadith024@gmail.com>
2025-03-30 23:50:02 -0600

    dashboard maybe is this idk

--- dashboard.py
+++ dashboard.py
@@ -1,21 +1,30 @@
 import streamlit as st
 import pandas as pd
+import requests
 
-# Cargar el dataset desde GitHub
-@st.cache_data
+def post_spark_job(user, repo, job, token, codeurl, dataseturl):
+   url = 'https://api.github.com/repos/' + user + '/' + repo + '/dispatches'
+   payload = {
+     "event_type": job,
+     "client_payload": {
+        "codeurl": codeurl,
+        "dataseturl": dataseturl
+      }
+   }
+   headers = {'Authorization': f'token {token}'}
+   response = requests.post(url, json=payload, headers=headers)
+   if response.status_code == 200:
+       st.success("Spark job triggered successfully!")
+   else:
+       st.error(f"Failed to trigger job: {response.status_code}")
 
-def load_data():
-    url = "dataset.csv"
+st.header("spark-submit Job")
 
-    df = pd.read_csv(url,nrows=100)
-    return df
-
-df = load_data()
-
-# Mostrar los datos en tabla
-st.title("📊 Dashboard de Datos")
-st.dataframe(df)
-
-
-
-st.success("✅ Dashboard listo con datos CSV")
+github_user  =  st.text_input('Github user', value='harlock024')
+github_repo  =  st.text_input('Github repo', value='stream_kafka_p')
+spark_job    =  st.text_input('Spark job', value='spark')
+github_token =  st.text_input('Github token', value='***')
+code_url     =  st.text_input('Code URL', value='')
+dataset_url  =  st.text_input('Dataset URL', value='')
+if st.button("POST spark submit"):
+   post_spark_job(github_user, github_repo, spark_job, github_token, code_url, dataset_url)
