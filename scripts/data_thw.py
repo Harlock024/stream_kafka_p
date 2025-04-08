edc59b59ebf54fb14bc9184fdceb55cb621221db
harlock024 <hadith024@gmail.com>
2025-04-08 00:34:04 -0600

    result fixed v3

--- scripts/data_thw.py
+++ scripts/data_thw.py
@@ -1,9 +1,9 @@
+
 from pyspark.sql import SparkSession
 import json
-import os
 
 if __name__ == "__main__":
-
+    # Crear sesión de Spark
     spark = SparkSession.builder \
         .appName("Música Processing") \
         .getOrCreate()
@@ -12,21 +12,21 @@ if __name__ == "__main__":
     path_music = "dataset.csv"
     df_music = spark.read.csv(path_music, header=True, inferSchema=True)
 
-
+    # Renombrar columna para uniformidad
     df_music = df_music.withColumnRenamed("track_name", "track_title")
 
-
+    # Filtrar canciones de la banda 'The Warning'
     df_warning_tracks = df_music.filter(df_music.artists.contains("The Warning"))
 
-
+    # Mostrar las canciones filtradas
     df_warning_tracks.show(20)
+
+    # Guardar los resultados en un archivo JSON
     results = df_warning_tracks.toJSON().collect()
+    df_warning_tracks.write.mode("overwrite").json("results")
 
-
-    output_file = 'results/data.json'
-    with open(output_file, 'w') as file:
+    with open("results/data.json","w") as file:
         json.dump(results, file)
-
-    print(f"Archivo guardado en {output_file}")
+    print(f"Archivo guardado en {file.name}")
 
     spark.stop()
