d886e37552aac358f42cb6dc574c5dd9354c7dcc
harlock024 <hadith024@gmail.com>
2025-04-07 23:42:53 -0600

    script data filter by the warning

--- dashboard.py
+++ dashboard.py
@@ -1,16 +1,16 @@
-from numpy import c_
 import streamlit as st
 import pandas as pd
 import requests
 import pymongo
 import psycopg2
-from sqlalchemy import create_engine
 import json
+
 
 @st.cache_resource
 def init_connection():
     return pymongo.MongoClient(st.secrets["mongo"]["uri"])
 client = init_connection()
+
 
 @st.cache_resource
 def init_connection_postgres():
@@ -46,54 +46,66 @@ def get_spark_results(url_results):
     else:
         st.error(f"Error {response.status_code}: No se pudieron obtener los datos.")
 
+def migrate_to_postgres():
+    url = "https://kafka-producer-postgres.onrender.com/trigger-producer/"
+    try:
+        response = requests.post(url)
+        if response.status_code == 200:
+            st.success("El proceso de Kafka ha sido activado correctamente.")
+        else:
+            st.error(f"Error al enviar a Kafka: {response.status_code}")
+    except Exception as e:
+        st.error(f"Ocurrió un error al intentar conectar con Kafka: {e}")
 
-def post_spark_job(user, repo, job, token, codeurl, dataseturl):    # Define the API endpoint
-    url = 'https://api.github.com/repos/' + user + '/' + repo + '/dispatches'
-    # Define the data to be sent in the POST request
+
+def post_spark_job(user, repo, job, token, code_url, dataset_url):
+    url = f'https://api.github.com/repos/{user}/{repo}/dispatches'
 
     payload = {
-     "event_type": job,
-     "client_payload": {
-        "codeurl": code_url,
-        "dataseturl": dataset_url
-     }
+        "event_type": job,
+        "client_payload": {
+            "codeurl": code_url,
+            "dataseturl": dataset_url
+        }
     }
-
     headers = {
-      'Authorization': 'Bearer ' + token,
-      'Accept': 'application/vnd.github.v3+json',
-      'Content-type': 'application/json'
+        'Authorization': f'Bearer {token}',
+        'Accept': 'application/vnd.github.v3+json',
+        'Content-type': 'application/json'
     }
 
-    st.write(url)
-    st.write(payload)
-    st.write(headers)
+    st.write(f"URL: {url}")
+    st.write(f"Payload: {payload}")
+    st.write(f"Headers: {headers}")
 
-    # Make the POST request
     response = requests.post(url, json=payload, headers=headers)
 
     st.write(response)
 
-
-st.title("Spark & streamlit")
+st.title("Spark & Streamlit")
 
 st.header("spark-submit Job")
 
-github_user  =  st.text_input('Github user', value='harlock024')
-github_repo  =  st.text_input('Github repo', value='stream_kafka_p')
-spark_job    =  st.text_input('Spark job', value='spark')
-github_token =  st.text_input('Github token', value='***')
+github_user  = st.text_input('Github user', value='harlock024')
+github_repo  = st.text_input('Github repo', value='stream_kafka_p')
+spark_job    = st.text_input('Spark job', value='spark')
+github_token = st.text_input('Github token', value='***')
 code_url = st.text_input('Code url', value='')
 dataset_url = st.text_input('Database url', value='')
 
 if st.button("POST spark submit"):
-    post_spark_job(github_user,github_repo,spark_job,github_token,code_url,dataset_url)
+    post_spark_job(github_user, github_repo, spark_job, github_token, code_url, dataset_url)
+
+
 st.header("Get Spark Results")
 
 url_results = st.text_input('Spark results url', value='')
 
 if st.button("GET spark results"):
     get_spark_results(url_results)
+
+if st.button("Migrate data to Postgres"):
+    migrate_to_postgres()
 
 if st.button("Query mongodb collection"):
     items = get_data_mongo()
---
+++ scripts/data_thw.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+    # Crear sesión de Spark
+    spark = SparkSession.builder \
+        .appName("Música Processing") \
+        .getOrCreate()
+
+    print("Leyendo dataset.csv ...")
+    path_music = "dataset.csv"
+    df_music = spark.read.csv(path_music, header=True, inferSchema=True)
+
+    # Renombrar columna para uniformidad
+    df_music = df_music.withColumnRenamed("track_name", "track_title")
+
+    # Filtrar canciones de la banda 'The Warning'
+    df_warning_tracks = df_music.filter(df_music.artists.contains("The Warning"))
+
+    # Mostrar las canciones filtradas
+    df_warning_tracks.show(20)
+
+    # Guardar los resultados en un archivo JSON
+    df_warning_tracks.toJSON().collect()  # Convertir a JSON
+    df_warning_tracks.write.mode("overwrite").json("results")
+
+    spark.stop()
