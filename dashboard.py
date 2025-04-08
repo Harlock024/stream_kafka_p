193a2d70b4527296a87942b4b200a009a9ac6292
harlock024 <hadith024@gmail.com>
2025-04-07 20:10:32 -0600

    dashboard updated

--- dashboard.py
+++ dashboard.py
@@ -5,6 +5,7 @@ import requests
 import pymongo
 import psycopg2
 from sqlalchemy import create_engine
+import json
 
 @st.cache_resource
 def init_connection():
@@ -23,11 +24,81 @@ def get_data_mongo():
     items = list(items)
     return items
 
+def get_spark_results(url_results):
+    if not url_results.startswith("http"):
+        st.error("La URL no es válida. Introduce una URL HTTP/HTTPS.")
+        return
+
+    response = requests.get(url_results)
+
+    if response.status_code == 200:
+        try:
+            json_lines = response.text.strip().split("\n")
+            data = [json.loads(line) for line in json_lines]
+
+            data = data[:100]
+
+            df = pd.DataFrame(data)
+            st.dataframe(df)
+
+        except json.JSONDecodeError as e:
+            st.error(f"Error al decodificar JSON: {e}")
+    else:
+        st.error(f"Error {response.status_code}: No se pudieron obtener los datos.")
+
+
+def post_spark_job(user, repo, job, token, codeurl, dataseturl):    # Define the API endpoint
+    url = 'https://api.github.com/repos/' + user + '/' + repo + '/dispatches'
+    # Define the data to be sent in the POST request
+
+    payload = {
+     "event_type": job,
+     "client_payload": {
+        "codeurl": code_url,
+        "dataseturl": dataset_url
+     }
+    }
+
+    headers = {
+      'Authorization': 'Bearer ' + token,
+      'Accept': 'application/vnd.github.v3+json',
+      'Content-type': 'application/json'
+    }
+
+    st.write(url)
+    st.write(payload)
+    st.write(headers)
+
+    # Make the POST request
+    response = requests.post(url, json=payload, headers=headers)
+
+    st.write(response)
+
+
+st.title("Spark & streamlit")
+
+st.header("spark-submit Job")
+
+github_user  =  st.text_input('Github user', value='harlock024')
+github_repo  =  st.text_input('Github repo', value='stream_kafka_p')
+spark_job    =  st.text_input('Spark job', value='spark')
+github_token =  st.text_input('Github token', value='***')
+code_url = st.text_input('Code url', value='')
+dataset_url = st.text_input('Database url', value='')
+
+if st.button("POST spark submit"):
+    post_spark_job(github_user,github_repo,spark_job,github_token,code_url,dataset_url)
+st.header("Get Spark Results")
+
+url_results = st.text_input('Spark results url', value='')
+
+if st.button("GET spark results"):
+    get_spark_results(url_results)
+
 if st.button("Query mongodb collection"):
     items = get_data_mongo()
     for item in items:
         st.write(item)
-
 
 postgres_uri = st.secrets["postgres"]["uri"]
 
